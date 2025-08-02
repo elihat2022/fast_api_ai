0352fefdc8b95d87d4a4166e3c2ee348453b6c90
Elihat Caceres <cacereselihat@gmail.com>
2025-08-02 11:58:14 -0500

    fast api gemma

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
 FROM python:3.13
 
+ARG HF_TOKEN
+ENV HUGGINGFACE_HUB_TOKEN=$HF_TOKEN
+
 COPY ./requirements.txt /web_app/requirements.txt
 
 WORKDIR /web_app
@@ -8,10 +11,11 @@ RUN pip install -r requirements.txt
 
 COPY web_app/* /web_app
 
-COPY web_app/roberta-sequence-classification-9.onnx /web_app
+# COPY web_app/roberta-sequence-classification-9.onnx /web_app
+
 
 
 ENTRYPOINT [ "uvicorn" ]
 
-CMD [ "--host", "0.0.0.0", "main:app"]
+CMD [ "--host", "0.0.0.0", "main:app", "--port", "8000"]
 
--- web_app/main.py
+++ web_app/main.py
@@ -2,10 +2,25 @@ from fastapi import FastAPI
 import torch
 import numpy as np
 from transformers import RobertaTokenizer
+from transformers import pipeline
 import onnxruntime
 from pydantic import BaseModel
+from huggingface_hub import login
+import os
+
 tokenizer = tokenizer = RobertaTokenizer.from_pretrained('roberta-base')
 session = onnxruntime.InferenceSession("roberta-sequence-classification-9.onnx")
+
+
+login(token=os.getenv("HUGGINGFACE_HUB_TOKEN"))
+pipe = pipeline(
+    "image-text-to-text",
+    model="google/gemma-3n-e2b-it",
+    device="cuda",
+    torch_dtype=torch.bfloat16,
+)
+
+
 
 app = FastAPI()
 class Body(BaseModel):
@@ -23,14 +38,36 @@ def root():
     """
     return {"message": "Welcome to the RoBERTa Sequence Classification API"}
 
-@app.post("/predict")
-def predict(body: Body):
-    """
-    curl -X POST --data '{"phrase": "This is a test"}' localhost:8000/predict
-    """
-    input_ids = torch.tensor(tokenizer.encode(body.phrase, add_special_tokens=True)).unsqueeze(0)
-    inputs = {session.get_inputs()[0].name: to_numpy(input_ids)}
-    out = session.run(None, inputs)
-    predict = np.argmax(out)
+# @app.post("/predict")
+# def predict(body: Body):
+#     """
+#     curl -X POST --data '{"phrase": "This is a test"}' localhost:8000/predict
+#     """
+#     input_ids = torch.tensor(tokenizer.encode(body.phrase, add_special_tokens=True)).unsqueeze(0)
+#     inputs = {session.get_inputs()[0].name: to_numpy(input_ids)}
+#     out = session.run(None, inputs)
+#     predict = np.argmax(out)
 
-    return {'positive': bool(predict)}
+#     return {'positive': bool(predict)}
+
+@app.post("/generate")
+def generate(body: Body):
+    """
+    curl -X POST --data '{"phrase": "This is a test"}' localhost:8000/generate
+    """
+
+    messages = [
+    {
+        "role": "system",
+        "content": [{"type": "text", "text": "You are a helpful assistant."}]
+    },
+    {
+        "role": "user",
+        "content": [
+            # {"type": "image", "url": "https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/p-blog/candy.JPG"},
+            {"type": "text", "text": body.phrase}
+        ]
+    }
+    ]
+    output = pipe(text=messages, max_new_tokens=200)
+    return {"generated_text": output[0]["generated_text"][-1]["content"]}
